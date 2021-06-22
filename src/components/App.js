08cf899247e210d98b7652d484407aae9c576243
Brian Smith <brian.bubbam@gmail.com>
2021-06-22 09:44:52 -0500

    commit tree dd483cf9c2a53700c037ada504bd36259e929ebe

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+
+import { useState, useEffect } from "react"
+
+
+function App() {
+        const [dogImage, setDogImage] = useState(null)
+
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then((dogImage => {
+          setDogImage(dogImage.message) 
+        }
+      )
+    )}, [])
+
+    if (!dogImage) return <p>Loading...</p>
+
+
+    return (
+        <div>
+            <img src={dogImage} alt="A Random Dog" />
+        </div>
+    )
+}
+
+export default App
